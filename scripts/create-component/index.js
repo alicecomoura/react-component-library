003735bb84c7cc5c2c1f9ea08c8f24f952aa74ff
Rian Fiore <74125852+Rianfiore@users.noreply.github.com>
2022-07-23 18:17:31 -0300

    - map and create props; - create folder before file; - export component at index.ts;

--- scripts/create-component/index.js
+++ scripts/create-component/index.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const path = "./testing-lib/";
+const types = require("./types");
 const fileExtensions = require("./data");
 const nameComponent = process.argv[2];
 const errorMessage = require("./error");
@@ -19,6 +19,7 @@ try {
 function createProps() {
   const command = process.argv;
   const props = [];
+  let newProps = [];
   const propsIndex = command.findIndex((argument) => argument === "--props");
 
   try {
@@ -33,30 +34,78 @@ function createProps() {
       }
 
       props.push(argument);
-      mappingProps(props);
     });
   } catch (e) {
     console.error(e);
   }
+  newProps = mappingProps(props);
 
-  console.log(props);
+  newProps.map((newProp) => {
+    newProp.value = convertType(newProp);
+  });
+
+  return newProps;
+}
+
+function convertType(prop) {
+  const type = prop.value.map((type) =>
+    type.type !== undefined ? `"${type.value}"` : `${type}`
+  );
+
+  const newType = type.join(" | ");
+  return newType;
 }
 
 function mappingProps(props) {
+  const propsArray = [];
   try {
     props.map((prop) => {
-      if (!prop.includes(":")) return errorMessage("propsAttribute", prop);
+      propsArray.push(validateProperty(prop));
     });
   } catch (e) {
     console.error(e);
   }
+  return propsArray;
 }
 
-function createFiles() {
+function validateProperty(prop) {
+  const propArray = prop.split(":");
+  const key = propArray[0];
+  const value = propArray[1];
+  if (!prop.match(/[A-Za-z0-9?]:[A-Za-z0-9]/)) {
+    if (prop.match(/[^\w\s]/)) return errorMessage("propsSpecialChar", prop);
+    return errorMessage("propsAttribute", prop);
+  }
+
+  const values = value.split(",");
+
+  const newValues = [];
+  values.map((value) => {
+    const type = types.find((type) => value.startsWith(type));
+
+    newValues.push(type !== undefined ? getTypeValue(value, type) : value);
+  });
+
+  function getTypeValue(value, type) {
+    let newValue = value.replace(type, "");
+
+    newValue.match(/[^\w\s,]/) && errorMessage("newValueSpecialChar", newValue);
+
+    newValue = newValue.match(/[,]/)
+      ? (newValue[0] === "," && errorMessage("newValueSpecialChar", newValue),
+        newValue.split(",")[0])
+      : newValue;
+
+    return { type: type, value: newValue };
+  }
+  return { prop: key, value: newValues };
+}
+
+function createFiles(folderPath, props) {
   fileExtensions.map((extensionName) => {
     fs.writeFile(
-      `${path}${extensionName(nameComponent, "file")}`,
-      extensionName(nameComponent, "data"),
+      `${folderPath}/${extensionName(nameComponent, "file")}`,
+      extensionName(nameComponent, "data", props),
       {
         encoding: "utf8",
         flag: "w",
@@ -64,21 +113,57 @@ function createFiles() {
       },
       (err) => {
         if (err) console.log(err);
-        else {
-          console.log("File written successfully\n");
-        }
       }
     );
   });
+
+  const newFolderPath = folderPath.replace(nameComponent, "index.ts");
+
+  exportOnIndex(newFolderPath, nameComponent);
+
+  console.log("Component created successfully");
+}
+
+function createFolder() {
+  const folderName = `./src/modules/design-system/components/${nameComponent}`;
+
+  try {
+    if (!fs.existsSync(folderName)) {
+      fs.mkdirSync(folderName);
+    } else {
+      errorMessage("folderExists");
+    }
+  } catch (err) {
+    console.error(err);
+  }
+
+  return folderName;
 }
 
 function addProps(props) {
+  const path = createFolder();
   switch (props) {
     case true:
-      createProps();
+      const props = createProps();
+      createFiles(path, props);
       break;
     case false:
-      createFiles();
+      createFiles(path);
       break;
   }
+}
+
+function exportOnIndex(path, nameComponent) {
+  const component = nameComponent
+    .split("-")
+    .map((name) => {
+      return name.charAt(0).toUpperCase() + name.slice(1);
+    })
+    .join("");
+
+  fs.appendFile(
+    path,
+    `\nexport { ${component} } from './${nameComponent}/${nameComponent}';`,
+    () => {}
+  );
 }
